8b9b1c4ef982f4e8ff449499ef7eeb2b2aed165c
Manoel Vilela <manoel.neto@neoway.com.br>
2024-05-17 20:51:14 -0300

    Re order filters

--- web/airdelay/main.py
+++ web/airdelay/main.py
@@ -8,27 +8,6 @@ from airdelay import flightmap
 
 # ## filters
 with st.sidebar.form("slider"):
-    start_date = datetime.today()
-    end_date = start_date.replace(year=start_date.year - 1)
-    # FIXME: esta resetando o range apos pressionar run
-    date_ranger_slider = st.slider(
-        "Selecione um intervalo de vôos",
-        min_value=start_date,
-        value=(start_date, end_date),
-        max_value=end_date,
-    )
-    espera_slider = st.slider(
-        "Espera em segundos",
-        min_value=0,
-        value=0,
-        max_value=4000,  # FIXME: use real values
-    )
-    max_simultaneous_flights = st.slider(
-        "Máximo de vôos simultâneos",
-        min_value=0,
-        value=3,
-        max_value=10,  # FIXME: use real values
-    )
     airports = ["SBSP", "SBMG"]
     destino_select = st.multiselect(
         label="Destino",
@@ -39,6 +18,28 @@ with st.sidebar.form("slider"):
         label="Origem",
         options=airports,
         default=None,
+    )
+
+    espera_slider = st.slider(
+        "Espera em segundos",
+        min_value=0,
+        value=0,
+        max_value=4000,  # FIXME: use real values
+    )
+    start_date = datetime.today()
+    end_date = start_date.replace(year=start_date.year - 1)
+    # FIXME: esta resetando o range apos pressionar run
+    date_ranger_slider = st.slider(
+        "Selecione um intervalo de vôos",
+        min_value=start_date,
+        value=(start_date, end_date),
+        max_value=end_date,
+    )
+    max_simultaneous_flights = st.slider(
+        "Máximo de vôos simultâneos",
+        min_value=0,
+        value=3,
+        max_value=10,  # FIXME: use real values
     )
     st.form_submit_button(label="Run")
 # ## filters
